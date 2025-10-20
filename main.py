c8680618c63bb4b774d0eb24f858011acb1e786b
salomao_machado <samachadosilva523@outlook.com>
2025-10-20 11:37:37 -0300

    commit

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ col1,col2 = st.columns(2)
 with col1:
     df = st.file_uploader("Carregar arquivo CSV",type="csv",help="Carregue o arquivo CSV do relatório exportado do SAP")
 if df is not None:
-    st.success("Arquivo carregado com sucesso!")
-    excel = conversor(df)
-    btn = st.download_button(
-        label= "Baixar arquivo Excel",
-        data = excel,
-        file_name="Base Pedido Atrasados.xlsx",
-        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        help="Faça download do arquivo excel convertido e classificado"
-    )
-    if btn:
-        st.success("Arquivo baixado com sucesso!")
+    with st.spinner("Convertendo arquivo..."):
+        excel = conversor(df)
+    if excel == None:
+        st.error("Erro ao converter arquivo")
+    else:
+        st.success("Conversão concluída com sucesso!")
+        st.write("Clique no botão abaixo para fazer o download do arquivo")
+        btn = st.download_button(
+            label= "Download",
+            data = excel,
+            file_name="Base Pedido Atrasados.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            help="Faça download do arquivo excel convertido e classificado"
+        )
+        if btn:
+            st.success("Arquivo baixado com sucesso!")
